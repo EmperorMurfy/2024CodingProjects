eb3e63a29d4828b26ee4f57c33ae2a70f684857a
Mason Z <110864180+EmperorMurfy@users.noreply.github.com>
2024-04-16 04:18:47 -0700

    Added recursiveFunction + PowerOf + info.md

---
+++ recursiveFunction/PowerOf.java
@@ -0,0 +1,50 @@
+// Name: Mason Z.
+// Program: PowerOf
+// Date: Apr 16, 2024
+// Description: powerOf function + superscript function
+
+import java.util.Scanner;
+
+public class PowerOf {
+
+
+	// run programs 
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("what is base value?");
+		int b = s.nextInt();
+		System.out.println("what is the exponent? (power of)? ");
+		int a = s.nextInt();
+
+		// System.out.println(b + " to the power of " + a );
+		System.out.println(b + superscript(a) + " is " + powerOf(b, a));
+	}
+	
+
+	// powerOf function 
+	// recursive function - learning example!
+	// b = base, a =  exponent
+	private static int powerOf(int b, int a) {
+		if (a == 0 )
+			return 1;
+		else 
+			return b * powerOf(b, a-1);
+	}
+	
+
+	// superScript function
+	public static String superscript(int n) {
+		// This array stores superscript characters for digits 0-9
+		char[] superscriptChars = {'⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'};
+		StringBuilder sb = new StringBuilder();
+		for (char c : String.valueOf(n).toCharArray()) {
+			sb.append(superscriptChars[c - '0']);
+		}
+		return sb.toString();
+	}
+	
+	
+
+
+
+}
---
+++ recursiveFunction/info.md.java
@@ -0,0 +1,15 @@
+# recursiveFunction
+Project written on Apr 16, 2024
+
+Contains one file, PowerOf
+
+Accepts two integer input in console, one as base, one as exponent
+Run through a powerOf function, which the recursive function that accepts these two inputs as b + a
+Replies with result when func end
+
+Function superscript accepts an n parameter, and will return with it's superscripted form
+The answer replied in main will print out b and a (a in superscript form) and the answer result by calling on powerOf function. 
+
+
+
+

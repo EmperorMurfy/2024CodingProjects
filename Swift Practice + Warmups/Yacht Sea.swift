ba6a5a8d022a38c71215bf41f97474d5f9d0c14d
Mason Z. <110864180+EmperorMurfy@users.noreply.github.com>
2024-09-13 14:20:23 -0400

    Update Yacht Sea.swift
     first working attempt

--- Swift Practice + Warmups/Yacht Sea.swift
+++ Swift Practice + Warmups/Yacht Sea.swift
@@ -1,13 +1,12 @@
 // Project : Yacht Sea
-// Written by: Mason Zhu
+// Written by: Mason Z
 // Date: Sept 13, 2024
-// Description:
-
-// work in progress
+// Description: Yacht Sea - Missing "4 of a kind, Small Straight, Big Straight, and Choice" Detection. Original approach.
+// note, full house will be recoded to use a set or dictionary instead of current approach as per goal - however, fully functional at said moment. 
 
 var highestScore: Int = 0 // highest score, default zero
-var category: String // category
-
+var category: String = "default" // category default temp placeholder
+var dice = [Int]()
 
 for _ in 1...20 {
     var array = [Int]()
@@ -19,22 +18,26 @@ for _ in 1...20 {
         array.append(number)
     }
     
-    // test
-    print(array)
+        // testing elements - removal is ok
+  //  print("Original Dice Roll")
+  //  print(array)
+
     
     // a Yacht Sea is where all values in the Array are equal values
     if (array.allSatisfy({ $0 == array.first }) == true) {
         highestScore = 50 // highest possible score
         category = "Yacht Sea"
+        dice = array
     }
     
     else if (array == [1, 2, 3, 4, 5] && highestScore < 30) { // conditions, if array fufills small straight, and that there isn't a higher score
         highestScore = 30
         category = "Small Straight"
+        dice = array
     }
     
     // create an array that checks in the original "array" length of 5, for the values 1 through 6
-
+    //           1  2  3  4  5  6
     var value = [0, 0, 0, 0, 0, 0]
     
     for i in 1...6 { // index 0
@@ -44,46 +47,37 @@ for _ in 1...20 {
             }
         }
     }
-    
-    print("value")
-    print(value)
-    print("")
-    
-    
-    
-    
-    // print("Array \(array) Yacht Sea Checker: \(array.allSatisfy({ $0 == array.first }))") // false if unfufilled
-    
-    
 
-
+    // testing elements - removal is ok
+ //   print("occurance")
+ //  print(value)
     
-/*
-var value = [Int]()
-    
-
-for n1 in array {
-    let i = n1
-    var sum: Int = 0
-    
-    // what if you instead removed elements, then find out how many numbers you are missing, that will be how many OCCURANCES happens. and you won't have more.
-    for n2 in array {
-        if (n2 == i) {
-            sum += 1
-        }
+    if (value.contains(2) && value.contains(3) && highestScore < 25) {
+        highestScore = 25
+        category = "Full House"
+        dice = array
     }
+  
+    // get all the occurances worth their value
+    for i in 1...6 {
+        value[i-1] *= i 
+    }
+     // testing elements - removal is ok   
+  //  print("value")
+   // print(value)
+   // print("")
     
-    array.removeAll(where: { $0 == i }) // remove all occurances of i
-    value.append(sum)
-        
-}
-                    
-print(value)
+    // find the max
+    let max: Int = value.max() ?? 0
+    let index: Int = array.firstIndex(of: max) ?? 0
     
-*/
-
+   let numerals = ["Ones", "Twos", "Threes", "Fours", "Fives", "Sixes"]
+    if (max > highestScore) {
+        highestScore = max
+        category = numerals[index]
+        dice = array
+    }
 }
-
-print("highest score \(highestScore)")
+print("input: \(dice) | output: \(highestScore) | explanation: \(category)")
 
 

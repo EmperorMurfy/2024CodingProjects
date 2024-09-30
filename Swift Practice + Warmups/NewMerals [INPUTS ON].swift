e1c57b55cd97cdbac1211c5a92a6844cdfb21a4a
Mason Z. <110864180+EmperorMurfy@users.noreply.github.com>
2024-09-29 21:11:09 -0400

    Add files via upload

---
+++ Swift Practice + Warmups/NewMerals [INPUTS ON].swift
@@ -0,0 +1,127 @@
+// Project : Newmerals
+// Written by: Mason Z
+// Date: Sept 29, 2024
+// Description: Newmerals + functions: join, promotion, mirror
+
+import Foundation
+
+print("welcome to new wave computer systems")
+print("Newmerals™ Software V1.0")
+print(" ")
+
+// Functions
+var join_closure : (String, String) -> String = {
+    var str: String = ""
+    let array1 = Array($0)
+    let array2 = Array($1)
+    
+    for i in 0..<array1.count { // assuming array1.count = array2.count
+    str.append(array1[i])
+    str.append(array2[i])
+    }
+    
+    return str
+}
+
+var promotion_closure : (String, String) -> String = {
+    var str: String = ""
+    let array1 = Array($0)
+    let array2 = Array($1)
+    
+    if (array1 == array2) {
+        for a in $0 {
+             // a, b, c, d
+            let compare: Array = ["A", "B", "C", "D"]
+            
+            
+            let index = compare.firstIndex(of: String(a))! + 1
+            
+            if (index < 3) {
+                str.append(compare[index])
+                
+            } else { // index of 4 - 0, 1, 2, 3, 4
+                str.append("A")
+            }
+        }
+        // place holder
+    }
+    
+    else {
+        for i in 0..<array1.count {
+            var compare = [array1[i], array2[i]] // ["A", "C"]
+            compare = compare.sorted(by: >)
+            str.append(compare[0])
+        }
+    }
+    
+    return str
+}
+
+var mirror_closure : (String, String) -> String = {
+    return String($1.reversed()) + $0 + $1
+}
+
+// Menu 
+var exit = 1
+
+while (exit == 1) {
+    print("************************************************************")
+    print("1 - Access Newmeral Interface")
+    print("0 - Exit Program")
+    print("************************************************************")
+    
+    // input, prevent input of non integers w/ error
+    var choice: Int = 0
+
+    if let c = Int(readLine()!) {  
+        choice = c
+    }   
+    
+    else {
+        print(" ")
+        print("ERROR 422: Invalid Input Type - Integers Only.")
+        
+        choice = 0
+    }
+   
+    switch choice {
+        case 0: // exit program
+            print("exiting program...")
+            exit = 0
+
+        case 1: // Access Newmeral Interface
+            print("Please input: [Value] [Operator] [Value]")
+            let newmerals: String = readLnine() ?? "AB : CD"
+
+            if newmerals.isEmpty {
+                print("Error 403: No Input Registered.")
+            } 
+            
+            else {
+                let array = newmerals.split(separator: " ")
+            
+                let numeralsA: String = String(array[0])
+                let numeralsB: String = String(array[2])
+            
+                if array[1] == ":" {
+                    print(join_closure(numeralsA, numeralsB))
+                } 
+                
+                else if array[1] == "^" {
+                    print(promotion_closure(numeralsA, numeralsB))
+                } 
+                
+                else if array[1] == "%" {
+                    print(mirror_closure(numeralsA, numeralsB))
+                } 
+                
+                else {
+                    print("Error 408: Operation Type \(array[1]) Not Found.")
+                }
+            }
+            
+            default: 
+                print(" ")
+                print("Error 422: Enter a valid input as stated.")
+    }
+}

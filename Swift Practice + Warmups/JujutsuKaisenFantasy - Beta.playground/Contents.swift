dc58579023bea35be7615348da4b0593747d0680
Mason Z. <110864180+EmperorMurfy@users.noreply.github.com>
2024-10-10 00:15:25 -0400

    Basic Functions Work! YAYYY

--- Swift Practice + Warmups/JujutsuKaisenFantasy - Beta.playground/Contents.swift
+++ Swift Practice + Warmups/JujutsuKaisenFantasy - Beta.playground/Contents.swift
@@ -5,6 +5,18 @@
 
 // no domains for now.
 
+// ✅ - working
+// ⚠️ - Being Developed (may work, but is not finished)
+// ❌ - Non-Functional or Error In Logic
+
+// CE - Cursed Energy
+// RCT - Reverse (Reversed) Curse Technique
+
+/* Heavenly Restriction - 
+ Debuffs: No Cursed Energy (RCT), No Ult. Hollow Purple will one shot no matter what.
+ Buffs:   Damage Absorbtion Increased - All attacks 75% of original damage
+          Inital Health Increase compared to Regular Special Grade
+ */
 
 import Foundation
 
@@ -18,7 +30,9 @@ class Player {
     var healing: Int
     var stun: Int
     
-    var status: Int { //var status: Int = 0 // -1 = dead, 0 = alive, 0 < x amount of rounds till back in action
+    var heavenlyRestriction = false // ✅
+    
+    var status: Int { // -1 = dead, 0 = alive, 0 < x amount of rounds till back in action ✅
         if (health < 0) {
             return -1
         } else {
@@ -26,13 +40,10 @@ class Player {
         }
     }
 
-    // [MainAttack, Secondary, Ult, Domain Expansion]
-    // Ult - Deal High Damage, Drains cursedEnergy based on Damage Level (Higher, the more it drains)
-    // Domain Expansion - Drains set cursedEnergy for all (75~60%), wider radius (meaning can damage more than one person), will set player out of comission for a few rounds
-
-    var grade: Int { // can't change
+    var grade: Int { // based on original set CE - doesn't change after set ✅
         let ranks = [4, 3, 2, 1, 0] // 0 = Special Grade
-        if (initialCursedEnergy == 0) { // Heavenly Restriction
+        if (initialCursedEnergy == 0) { // Heavenly Restriction - Automatically Buffs
+            heavenlyRestriction = true
             return ranks[4]
         }
         else {
@@ -40,11 +51,16 @@ class Player {
         }
     }
     
-    var health: Int {
-        return ((125 - grade * 25) + healing) - damage
+    var health: Int { // amount of health - updates when changed ✅
+        if (heavenlyRestriction == true) {
+            ((125 - grade * 25) + healing + 35) - Int(0.75 * Double(damage))
+        }
+        else {
+            ((125 - grade * 25) + healing) - damage
+        }
     }
     
-    init(name: String = "default", cursedEnergy: Double = 10.0, cursedTechnique: [String : [Double]] = [:], damage: Int = 0, healing: Int = 0, stun: Int = 0) {
+    init(name: String = "default", cursedEnergy: Double = 10.0, cursedTechnique: [String : [Double]] = ["Reversal Red": [20.0, 0.0], "Lapse Blue": [5.0, 3.0], "Hollow Purple": [45.5, 2.0]], damage: Int = 0, healing: Int = 0, stun: Int = 0) {
         self.name = name
         self.cursedEnergy = cursedEnergy
         self.cursedTechnique = cursedTechnique
@@ -54,7 +70,7 @@ class Player {
         self.initialCursedEnergy = cursedEnergy // remains the same as original input. so CursedTechnique can change while this will remain the same.
     }
     
-    func replenishCE() { // 380 < 400
+    func replenishCE() { // 380 < 400 ✅
         if (cursedEnergy < initialCursedEnergy) {
             cursedEnergy+=0.1*initialCursedEnergy
             if (cursedEnergy > initialCursedEnergy) { // if overflow, added too much cursed energy (like inital 400, added to 430 from 390)
@@ -63,7 +79,8 @@ class Player {
         }
         // all other condition is that 400 = 400 - cursed energy level is fine
     }
-    func reverseCurseTechnique() { //random value between 50% of all current cursed energy to 5% of all current cursed energy - 1HP translates to around 1.2 cursed energy - How to heal allies using excess?
+    func reverseCurseTechnique() -> String { // ✅
+        //random value between 50% of all current cursed energy to 5% of all current cursed energy - 1HP translates to around 1.2 cursed energy - How to heal allies using excess?
         let usedCE = Double.random(in: (0.2 * cursedEnergy)...(0.5 * cursedEnergy))
         cursedEnergy -= usedCE
         let healAmount: Int = Int(usedCE/1.2)
@@ -72,15 +89,46 @@ class Player {
         
         // 70 + 50 = 120
         healing += healAmount
-        print("Reversed Cursed Technique Applied \((healAmount)) to \(name)")
+        let report = "RCT: \(healAmount) HP"
+        
         if (health > maxHealth) {
             let excess = health - maxHealth // excess
             cursedEnergy += Double(excess) * 1.2
             healing = healing-excess // remove excess
         }
+
+        return report
+        
     }
     
-    
+    func techniques() -> (damage: Int, stun: Int, report: String){
+        var technique = (0, 0, "")
+        
+        // cursedTechnique: ["Reversal Red": [20.0, 0.0], "Lapse Blue": [5.0, 3.0], "Hollow Purple": [45.5, 2.0]]
+        if (grade < 3 && Int.random(in: 0...1000000) == 271) { // BLACK FLASH
+            technique = (71, 4, "ϐׁׅ֒ᥣׁׅ֪ɑׁׅ֮ᝯׁƙׁׅ֑ ⨍ᥣׁׅ֪ɑׁׅ֮꯱ׁׅ֒hׁׅ֮!!!")
+        } else {
+            let attacks = Array(cursedTechnique.keys)
+
+            let attack = attacks.randomElement()
+
+            if let attack = attack {
+                let attackStats = cursedTechnique[attack]
+
+                if let attackStats = attackStats {
+                    let energyCost = attackStats[2]
+
+                    if energyCost < cursedEnergy {
+                        technique = (Int(attackStats[0]), Int(attackStats[1]), attack)
+                    } else {
+                        technique = techniques() // if we roll an ult, and don't have enough.
+                    }
+                }
+            }
+        }
+        return technique
+    }
+         
     
     // all ults require at least 55% of all cursed technique - domain expansion 75%
     func action() -> (damage: Int, stun: Int, report: String) {
@@ -88,35 +136,31 @@ class Player {
         let choice = Int.random(in: 1...2)
         
         if (choice == 1) {
-            actions = (10, 0, "Attack") // add atack function. 
+            actions = techniques()
+            // actions = (10, 0, "Attack") // add atack function.
         }
         else if (choice == 2) { //Heal - random value between 75% of all current cursed energy to 5% of all current cursed energy - 1HP translates to around 1.2 cursed energy
             let maxHealth = maxHealth()
             if (grade < 2 && Int(cursedEnergy) != 0 && Int(initialCursedEnergy) != 0 && maxHealth != health) { // must be grade 1 or up, must have more than 0 cursedEnergy, cannot have heavenly restriction, if health is 100%, won't get heal
-                reverseCurseTechnique()
-                actions = (0, 0, "Used RTC, Skipped Attack")
+            
+                let report = reverseCurseTechnique()
+                actions = (0, 0, report)
             } else {
                 actions = action() // if cannot use RTC, reroll.
             }
         }
         return actions
     }
-     
-    /*
     
-   
-    func domainExpansion() -> (Int, String) {
-        
-    }
-     */
     
-    func maxHealth() -> Int {
+    func maxHealth() -> Int { // ✅
         return 125 - grade * 25
     }
         
-    func statusReport() -> String { // -1 = dead, 0 = alive, 0 < x amount of rounds till back in action
+    func statusReport() -> String { // ✅
+        // -1 = dead, 0 = alive, 0 < x amount of rounds till back in action
         if (status > 0) {
-            return "Currently knocked out for \(status) rounds"
+            return "Currently knocked out for \(status) rounds + \(health) HP Left"
         } else if (status == -1){
             return "Terminated"
         } else {
@@ -125,7 +169,7 @@ class Player {
     }
     
     // return cursedEnergypercentage
-    func cursedEnergyPercentage() -> String {
+    func cursedEnergyPercentage() -> String { //✅
         if (initialCursedEnergy == 0) {
             return "Heavenly Restriction - No Cursed Energy"
         }else {
@@ -133,7 +177,7 @@ class Player {
         }
     }
     
-    func ranking() -> String {
+    func ranking() -> String { //✅
         var rank: String = ""
         if (grade == 0) {
             rank.append("Special Grade")
@@ -142,7 +186,6 @@ class Player {
         }
         return rank
     }
-    
 }
 
 class Team {
@@ -164,33 +207,53 @@ class Team {
         self.players = players
     }
     
-    func turn() -> (damage: Int, stun: Int, report: String) { // output a certain damage, stun, and report
+    func turn() -> (damage: Int, stun: Int, report: String) { // ✅
+        // output a certain damage, stun, and report: (heal, attack: "ability")
         let randomPlayer = activePlayers.randomElement()!
         let result = randomPlayer.action()
-
+        recover()
+        
         return (damage: result.damage, stun: result.stun, report: "\(randomPlayer.name) - \(result.report)")
     }
     
-    func receive(damage: Int, stun: Int) -> Bool { // check if win or lost, take the damge
-        
+    func receive(damage: Int, stun: Int, report: String) -> Bool { // ✅
+        // check if win or lost, take the damge
         if (activePlayers.isEmpty) {
             teamReport()
             return true
         } else {
             let randomPlayer = activePlayers.randomElement()! // pick random player
             
-            randomPlayer.damage = damage
-            randomPlayer.stun = stun
+            if (randomPlayer.heavenlyRestriction == true && report == "Satoru Gojo - Hollow Purple") { // heavenlyRestriction - hollow purple guaranteed one shot.
+                randomPlayer.damage = 100000
+                randomPlayer.stun = -1
+            }
             
-            teamReport()
+            else {
+                randomPlayer.damage = damage
+                randomPlayer.stun = stun
+            }
+            teamReport() // report stats
+            
+            recover()
             return false
         }
     }
+    
+    func recover() { // ✅
+        for player in players {
+            player.replenishCE() // recover cursedEnergy after
+            if (player.stun > 0) {
+                player.stun -= 1
+            }
+            
+        }
+    }
 
-    func teamReport() { // report entire team
+    func teamReport() { //
+        // return reports regarding a entire team on a certain round
         var names = [String]()
         for player in players { // add to list
-            player.replenishCE()
             names.append("\(player.name): \(player.ranking()) Sorcerer\nStatus: \(player.statusReport())\n\(player.cursedEnergyPercentage()) \n")
         }
         print("************************************************************")
@@ -201,40 +264,25 @@ class Team {
     }
 }
 
-let gojoSatoru = Player(name: "Satoru Gojo", cursedEnergy: 400.00, cursedTechnique: ["Reversal Red": [20.0, 0.0], "Lapse Blue": [5.0, 3.0], "Hollow Purple": [45.5, 2.0]])
+// add one passive ability to all characters that requires no cursed energy
+let gojoSatoru = Player(name: "Satoru Gojo", cursedEnergy: 400.00, cursedTechnique: ["Reversal Red": [20, 0, 0], "Lapse Blue": [12, 3, 0], "Hollow Purple": [45, 4, 70]])
 
-let sukunaRyomen = Player(name: "Ryomen Sukuna", cursedEnergy: 395.50, cursedTechnique: ["Dismantle": [22.5, 0.0], "Cleave": [15.0, 2.0], "Divine Flame": [35.5, 3.0]])
+let sukunaRyomen = Player(name: "Ryomen Sukuna", cursedEnergy: 395.50, cursedTechnique: ["Dismantle": [24, 0, 0], "Cleave": [15, 2, 0], "Divine Flame": [35, 6, 70]])
 
-let itadoriYuji = Player(name: "Yuji Itadori", cursedEnergy: 335.00, cursedTechnique: ["Divergent Fist": [20.0, 0.0], "Dismantle": [12.5, 0.0], "Black Flash!": [30.0, 4.0]])
+let itadoriYuji = Player(name: "Yuji Itadori", cursedEnergy: 335.00, cursedTechnique: ["Divergent Fist": [20, 0, 0], "Piercing Blood": [10, 1, 0], "Dismantle": [24, 2, 70]])
 
 
-let fushiguroToji = Player(name: "Tojo Fushiguro", cursedEnergy: 0.0, cursedTechnique: ["splitSoulKatana": [18.0, 0.0], "invertedSpearofHeaven": [20.5, 3.0], "Pistol": [38.2, 2.0]])
+let fushiguroToji = Player(name: "Tojo Fushiguro", cursedEnergy: 0.0, cursedTechnique: ["splitSoulKatana": [22, 0, 0], "invertedSpearofHeaven": [20, 3, 0], "Pistol": [38, 0, 0]])
 
 let team1 = Team(name: "The Super Seniors", players: [gojoSatoru, sukunaRyomen])
 let team2 = Team(name: "Tokyo Jujutsu High", players: [itadoriYuji, fushiguroToji])
 
-team2.teamReport()
+
 
 let firstTurn = team1.turn()
-print(firstTurn)
+print(firstTurn.report)
 
-team2.receive(damage: firstTurn.damage, stun: firstTurn.stun)
-
-
-/*
-team1.teamReport()
-
-team1.players[2].damage = 120
-team1.players[0].cursedEnergy = 355
-
-for player in team1.activePlayers {
-    print(player.name)
-}
-
-team1.teamReport()
-
-// team1.players[0].cursedEnergy = 0.2
- */
-
-
-
+team2.receive(damage: firstTurn.damage, stun: firstTurn.stun, report: firstTurn.report)
+print(firstTurn.damage)
+print(team2.players[1].health)
+print("hi")
